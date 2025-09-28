6d5d0a989adb2a9e72c717e6216c86767090c74c
xiao-sudo <85089925@qq.com>
2025-09-28 18:37:44 +0800

    1. add UIEventMask

---
+++ Assets/Scripts/UIExt/Base/UIEventMask.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIExt.Base
+{
+    [RequireComponent(typeof(UIEventPassThrough))]
+    public class UIEventMask : Image
+    {
+        public enum MaskType
+        {
+            Rect,
+            Circle
+        }
+
+        public enum PassThroughType
+        {
+            /// <summary>
+            /// Pass in target's rect
+            /// </summary>
+            PassAtTargetRect,
+
+            /// <summary>
+            /// Pass outside target's rect
+            /// </summary>
+            PassExcludeTargetRect,
+
+            /// <summary>
+            /// Pass always
+            /// </summary>
+            PassAlways,
+
+            /// <summary>
+            /// Pass Never
+            /// </summary>
+            PassNever
+        }
+
+        [SerializeField]
+        private UIEventPassThrough m_EventPassThrough;
+
+        [SerializeField]
+        private PassThroughType m_PassThroughType = PassThroughType.PassAtTargetRect;
+
+        [SerializeField]
+        private MaskType m_MaskType = MaskType.Rect;
+
+        private GameObject m_MaskTarget;
+        private RectTransform m_MaskTargetTransform;
+
+        public PassThroughType PassThroughStyle
+        {
+            get => m_PassThroughType;
+            set => m_PassThroughType = value;
+        }
+
+        public MaskType MaskStyle
+        {
+            get => m_MaskType;
+            set => m_MaskType = value;
+        }
+
+        /// <summary>
+        /// Passed click event is executed
+        /// </summary>
+        public Action<GameObject> PassThroughClickCallback
+        {
+            set => EventPassThrough.PassThroughClickCallback = value;
+        }
+
+        private UIEventPassThrough EventPassThrough
+        {
+            get
+            {
+                if (null == m_EventPassThrough)
+                    m_EventPassThrough = GetComponent<UIEventPassThrough>();
+
+                return m_EventPassThrough;
+            }
+        }
+
+        public void SetMaskTarget(GameObject target)
+        {
+            if (null == target)
+            {
+                Debug.LogError("Hollow Target is null");
+                return;
+            }
+
+            m_MaskTarget = target;
+            m_MaskTargetTransform = m_MaskTarget.GetComponent<RectTransform>();
+
+            EventPassThrough.PassThroughTarget = m_MaskTarget;
+        }
+
+        public void SetMask(MaskType maskType)
+        {
+        }
+
+        public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+        {
+            switch (m_PassThroughType)
+            {
+                case PassThroughType.PassAtTargetRect:
+                    var r = RectTransformUtility.RectangleContainsScreenPoint(m_MaskTargetTransform, screenPoint,
+                        eventCamera);
+
+                    return !r;
+
+                case PassThroughType.PassExcludeTargetRect:
+                    return RectTransformUtility.RectangleContainsScreenPoint(m_MaskTargetTransform, screenPoint,
+                        eventCamera);
+
+                case PassThroughType.PassAlways:
+                    return false;
+
+                case PassThroughType.PassNever:
+                    return true;
+
+                default:
+                    return true;
+            }
+        }
+
+        public Rect HollowTargetRectOnScreenSpace
+        {
+            get
+            {
+                var rect = Utility.UIRect.GetRectInScreenSpace(m_MaskTargetTransform, canvas.GetComponent<Camera>());
+                return rect;
+            }
+        }
+
+        public RectTransform MaskTargetRectTransform => m_MaskTargetTransform;
+    }
+}
---
+++ Assets/Scripts/UIExt/Base/UIEventPassThrough.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Pool;
+
+namespace UIExt.Base
+{
+    public class UIEventPassThrough : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+    {
+        private readonly struct EventExecuteTarget
+        {
+            private readonly bool m_Executed;
+            private readonly GameObject m_Target;
+
+            public EventExecuteTarget(bool executed, GameObject target)
+            {
+                m_Executed = executed;
+                m_Target = target;
+            }
+
+            public bool Executed => m_Executed;
+            public GameObject Target => m_Target;
+        }
+
+        [SerializeField]
+        private GameObject m_PassThroughTarget;
+
+        private Action<GameObject> m_PassThroughClickCallback;
+
+        private bool IsSpecifiedTarget => null != m_PassThroughTarget;
+
+        public GameObject PassThroughTarget
+        {
+            get => m_PassThroughTarget;
+            set => m_PassThroughTarget = value;
+        }
+
+        public Action<GameObject> PassThroughClickCallback
+        {
+            get => m_PassThroughClickCallback;
+            set => m_PassThroughClickCallback = value;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            var executeTarget = PassEvent(eventData, ExecuteEvents.pointerClickHandler);
+
+            if (executeTarget.Executed)
+                m_PassThroughClickCallback.Invoke(executeTarget.Target);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            PassEvent(eventData, ExecuteEvents.pointerDownHandler);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            PassEvent(eventData, ExecuteEvents.pointerUpHandler);
+        }
+
+        private EventExecuteTarget PassEvent<T>(PointerEventData eventData, ExecuteEvents.EventFunction<T> handler)
+            where T : IEventSystemHandler
+        {
+            if (!IsSpecifiedTarget)
+                return new EventExecuteTarget(false, null);
+
+            var results = ListPool<RaycastResult>.Get();
+
+            EventSystem.current.RaycastAll(eventData, results);
+
+            bool executed = false;
+            GameObject executeGo = null;
+
+            foreach (var result in results)
+            {
+                var go = result.gameObject;
+
+                executed = ExecuteOnlyOnSpecifiedTarget(go, eventData, handler);
+
+                if (executed)
+                {
+                    executeGo = go;
+                    break;
+                }
+            }
+
+            ListPool<RaycastResult>.Release(results);
+
+            return new EventExecuteTarget(executed, executeGo);
+        }
+
+        private bool ExecuteOnlyOnSpecifiedTarget<T>(GameObject go, PointerEventData eventData,
+            ExecuteEvents.EventFunction<T> handler) where T : IEventSystemHandler
+        {
+            if (go == m_PassThroughTarget)
+                return ExecuteEvents.Execute(go, eventData, handler);
+
+            return false;
+        }
+    }
+}
